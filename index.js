2103fca082e0fa256ef67ce77afb9f4ed0abb08a
Victor Haynes <haynesvic@gmail.com>
2022-06-28 09:54:41 -0500

    commit tree cce086548c9b30ec3a217dc6441b3abb1204ac27

---
+++ index.js
@@ -0,0 +1,8 @@
+const heading = document.querySelector("h1")
+console.log(heading)
+
+heading.innerText = "Flatiron School is the best school!";
+
+const paragraph = document.getElementById("paragraph");
+console.log(paragraph);
+paragraph.remove();
